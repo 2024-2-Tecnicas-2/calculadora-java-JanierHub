fe5289190b67ac7e6d6541f86e4747e597e608f3
Name <correo@gmai.com>
2024-08-21 19:19:15 -0500

    Cambios Modulo y Potencia java

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -27,7 +27,15 @@ public class Calculadora {
                 respuesta = (double) numero1 / numero2;
                 break;
             
-            // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
+            case '^' :
+                // Operación potencia:
+                respuesta = Math.pow(numero1, numero2);
+                break;
+                
+            case '%' :
+                // Operación modulo:
+                respuesta =  numero1 % numero2;
+                break;
                 
             default:
                 throw new IllegalArgumentException("Operación inválida.");
@@ -46,7 +54,7 @@ public class Calculadora {
         
         // Lectura de la operación a realizar
         // TODO: DEBES ADICIONAR EL SÍMBOLO DE ^ Y % PARA CALCULAR LA POTENCIA Y EL MÓDULO
-        System.out.println("Ingrese la operación (+, -, *, /)");
+        System.out.println("Ingrese la operación (+, -, *, /,^,%)");
         char operacion = lector.next().charAt(0);
         
         double respuesta = calcular(numero1, numero2, operacion);
